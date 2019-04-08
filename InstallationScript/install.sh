5760ea0615fdfe1253f8f1098ac60bbe5f819266
CycleUser <cycleuser@cycleuser.org>
2019-04-08 08:23:07 +0800

    Create install.sh

---
+++ InstallationScript/install.sh
@@ -0,0 +1,11 @@
+sudo su - -c "R -e \"install.packages('devtools',repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('rmarkdown',repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"devtools::install_github('daattali/shinyjs',force=TRUE)\""
+sudo su - -c "R -e \"install.packages('shiny',repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotR',force=TRUE)\""
+sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotRgui',force=TRUE)\""
+
+
+
+
+
