4c00c3b5fc7a1dad976b9be8e32463ce20c9e1f3
CycleUser <cycleuser@cycleuser.org>
2019-04-10 01:00:45 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,5 +1,12 @@
+deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/
+
 wget https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu/bionic-cran35/r-base-core_3.5.3-1bionic_i386.deb
-git clone https://github.com/pvermees/IsoplotR
-git clone https://github.com/pvermees/IsoplotRgui
-git clone https://github.com/chinageology/IsoplotRgui-CN
+wget https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu/bionic-cran35/r-base-core_3.5.3-1bionic_amd64.deb
+wget https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu/bionic-cran35/r-base-dev_3.5.3-1bionic_all.deb
+
+
+
+#git clone https://github.com/pvermees/IsoplotR
+#git clone https://github.com/pvermees/IsoplotRgui
+#git clone https://github.com/chinageology/IsoplotRgui-CN
 
