89ec86dab5e3034c91fbd7e743c06265d7483b06
CycleUser <cycleuser@cycleuser.org>
2019-04-11 12:07:37 +0800

    Update install.sh

--- InstallationScript/install.sh
+++ InstallationScript/install.sh
@@ -25,11 +25,6 @@ sudo su - -c "R -e \"install.packages('processx',repos='http://cran.rstudio.com/
 sudo su - -c "R -e \"install.packages('devtools',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('shiny',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('rmarkdown',repos='http://cran.rstudio.com/')\""
-
-
-
-
-
 sudo su - -c "R -e \"devtools::install_github('daattali/shinyjs',force=TRUE)\""
 sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotR',force=TRUE)\""
 sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotRgui',force=TRUE)\""
