3c9fff94e788c00d7b9644e2d39ce1e8f303b2f7
CycleUser <cycleuser@cycleuser.org>
2019-04-11 13:30:53 +0800

    Update install.sh

--- InstallationScript/install.sh
+++ InstallationScript/install.sh
@@ -22,12 +22,16 @@ sudo su - -c "R -e \"install.packages('ps',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('fs',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('glue',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('processx',repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('devtools',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('promises',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('httpuv',repos='http://cran.rstudio.com/')\""
+sudo su - -c "R -e \"install.packages('mime',repos='http://cran.rstudio.com/')\""
+
+
+
+sudo su - -c "R -e \"install.packages('devtools',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('shiny',repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('rmarkdown',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"devtools::install_github('daattali/shinyjs',force=TRUE)\""
+sudo su - -c "R -e \"install.packages('rmarkdown',repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotR',force=TRUE)\""
 sudo su - -c "R -e \"devtools::install_github('pvermees/IsoplotRgui',force=TRUE)\""
 
